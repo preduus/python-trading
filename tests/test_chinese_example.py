707e685d6658d2685c30e734b92632232724330b
Pedro Rodrigues <pedro.rodrigues@hfocus.com.br>
2022-01-16 13:39:03 -0300

    Added project tests layer

---
+++ tests/test_chinese_example.py
@@ -0,0 +1,27 @@
+import unittest
+
+import pandas
+
+from examples.chinese import ChineseExample
+
+
+class TestChineseStrategy(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.strategy = ChineseExample()
+
+    def test_get_stock_data(self):
+        self.assertIsInstance(self.strategy.get_stock_data(), list, "Stock data type list. ok!")
+        self.assertTrue(len(self.strategy.get_stock_data()) > 0, "Stock data is not empty. ok!")
+
+    def test_create_dataframe(self):
+        self.strategy.create_dataframe()
+        self.assertIsInstance(self.strategy.dataframe, pandas.DataFrame, "DataFrame initialization. ok!")
+        self.assertTrue(len(self.strategy.dataframe.index) > 0, "DataFrame data is not empty. ok!")
+
+    def test_strategy(self):
+        self.assertIsInstance(self.strategy.strategy(), tuple, "Strategy classmethod returns correct type. ok!")
+
+
+if __name__ == "__main__":
+    unittest.main()
